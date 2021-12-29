b4c787cd93b94d1499d5f7e192c05e02901af2fe
glemin5011 <59791512+glemin5011@users.noreply.github.com>
2021-12-29 15:05:47 +0100

    Initial commit

---
+++ apps/web/controllers/demo/show.rb
@@ -0,0 +1,12 @@
+module Web
+  module Controllers
+    module Demo
+      class Show
+        include Web::Action
+
+        def call(params)
+        end
+      end
+    end
+  end
+end
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  important: true,
+  content: [
+    "./apps/web/assets/javascripts/**/*.{html,js,jsx}",
+    "./apps/web/templates/**/*.html.erb",
+    "./public/**/*.{html, js}",
+    "./*.html",
+  ],
+  jit: true,
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
